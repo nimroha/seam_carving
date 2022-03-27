fixed direction of optimal seam calculation

seam_carving.py
@@ -17,44 +17,44 @@ def compute_gradient_magnitude(intensity):
     return np.sqrt((grad_x ** 2 + grad_y ** 2) / 2)
 
 
-def get_left_neighbors_min_value(arr, i, j):
+def get_top_neighbors_min_value(arr, i, j):
     """
-    get the minimal value of the 3 left neighbors of the pixel (i,j)
+    get the minimal value of the 3 top neighbors of the pixel (i,j)
 
     :param arr: numpy array of values
     :param i: row index
     :param j: col index
 
-    :return: minimal value of left neighbors
+    :return: minimal value of top neighbors
     """
-    if j == 0:
+    if i == 0:
         return 0 # no left neighbors
 
     # get "in-bounds" rows of neighbors
     h, w = arr.shape[:2]
-    neighbor_rows = [max(0, i - 1),
-                     i,
-                     min(h - 1, i + 1)]
+    neighbor_cols = [max(0, j - 1),
+                     j,
+                     min(w - 1, j + 1)]
 
-    values = [arr[row, j - 1] for row in neighbor_rows] # values can repeat but it doesn't matter since we take the mean
+    values = [arr[i - 1, col] for col in neighbor_cols] # values can repeat but it doesn't matter since we take the mean
 
     return np.min(values)
 
 
-def horizontal_forward_energy(grad):
+def horizontal_vertical_energy(grad):
     raise NotImplementedError()
 
 
-def horizontal_backward_energy(grad):
+def vertical_backward_energy(grad):
     energy = grad.copy()
-    for j in range(energy.shape[1]):
-        for i in range(energy.shape[0]):
-            energy[i, j] += get_left_neighbors_min_value(energy, i, j)
+    for i in range(energy.shape[0]):
+        for j in range(energy.shape[1]):
+            energy[i, j] += get_top_neighbors_min_value(energy, i, j)
 
     return energy
 
 
-def select_horizontal_seams(energy, k):
+def select_vertical_seams(energy, k):
     """
     select horizontal seams
 
@@ -67,7 +67,7 @@ def select_horizontal_seams(energy, k):
     # TODO select greedily or all at once
 
 
-def horizontal_resize(image, k, use_forward):
+def resize_cols(image, k, use_forward):
     """
     find horizontal seams and remove/duplicate them
 
@@ -80,7 +80,7 @@ def horizontal_resize(image, k, use_forward):
     """
     intensity = utils.to_grayscale(image)
     gradient  = compute_gradient_magnitude(intensity)
-    energy    = horizontal_forward_energy(gradient) if use_forward else horizontal_backward_energy(gradient)
+    energy    = horizontal_vertical_energy(gradient) if use_forward else vertical_backward_energy(gradient)
 
     return image, image
 
@@ -100,8 +100,8 @@ def resize(image: NDArray, out_height: int, out_width: int, forward_implementati
     height_diff = out_height - image.shape[0]
     width_diff  = out_width  - image.shape[1]
 
-    horizontal_carved,   horizontal_seams   = horizontal_resize(image,                       width_diff,  forward_implementation)
-    vertical_carved_rot, vertical_seams_rot = horizontal_resize(np.rot90(horizontal_carved), height_diff, forward_implementation)
+    horizontal_carved,   horizontal_seams   = resize_cols(image, width_diff, forward_implementation)
+    vertical_carved_rot, vertical_seams_rot = resize_cols(np.rot90(horizontal_carved), height_diff, forward_implementation)
     vertical_carved = np.rot90(vertical_carved_rot, -1)
     vertical_seams  = np.rot90(vertical_seams_rot,  -1)
 

tests.py
@@ -1,7 +1,7 @@
 import pytest
 import numpy as np
 
-from seam_carving import horizontal_backward_energy
+from seam_carving import vertical_backward_energy
 
 
 def test_horizontal_backward_energy():
@@ -14,6 +14,6 @@ def test_horizontal_backward_energy():
                                 [ 9,  7,  6, 12],
                                 [14,  9, 10,  8],
                                 [14, 13, 15, 16]])
-    energy = np.rot90(horizontal_backward_energy(np.rot90(grad)), -1) # rotated because we implemented it horizontally
+    energy = vertical_backward_energy(grad)
 
     assert np.all(energy == expected_energy), {'got': energy, 'expected': expected_energy}