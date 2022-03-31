7d31277c8d4603d5fe037d485181e5dd22ef5f6a
nimrodm <nimrod@voyage81.com>
2022-03-31 11:02:18 +0300

    added tests

--- tests.py
+++ tests.py
@@ -1,10 +1,19 @@
+import os
 import pytest
 import numpy as np
 
-from seam_carving import vertical_backward_cost
+from seam_carving import vertical_backward_cost, roll_matrix, remove_seam, resize
+from utils import open_image
 
+TEST_IMAGE_PATH   = os.path.join(os.path.dirname(__file__), 'checkpoint.jpeg')
+TEST_IMAGE_HEIGHT = 262
+TEST_IMAGE_WIDTH  = 400
 
-def test_horizontal_backward_energy():
+@pytest.fixture(scope='session')
+def test_img():
+    return open_image(TEST_IMAGE_PATH)
+
+def test_vertical_backward_energy():
     grad = np.array([[5, 8, 12, 3],  # from the lecture slides
                      [4, 2,  3, 9],
                      [7, 3,  4, 2],
@@ -17,3 +26,34 @@ def test_horizontal_backward_energy():
     cost, min_indices = vertical_backward_cost(grad)
 
     assert np.all(cost == expected_energy), {'got': cost, 'expected': expected_energy}
+
+
+def test_roll_matrix():
+    arr = np.eye(5)
+    roll_by = np.arange(5)
+    rolled = roll_matrix(arr, -roll_by)
+    assert np.all(rolled[:,1:] == 0)
+    assert np.all(rolled[:, 0] == 1)
+
+
+def test_remove_seam():
+    arr = np.eye(5)
+    seam_cols = np.arange(5)
+    removed = remove_seam(arr, seam_cols)
+    assert arr.shape[1] == removed.shape[1] + 1 # removed one column
+    assert arr.shape[0] == removed.shape[0]     # did not remove rows
+    assert np.all(removed == 0)
+
+
+@pytest.mark.parametrize('h', [TEST_IMAGE_HEIGHT, TEST_IMAGE_HEIGHT + 30, TEST_IMAGE_HEIGHT - 30])
+@pytest.mark.parametrize('w', [TEST_IMAGE_WIDTH,  TEST_IMAGE_WIDTH  + 30, TEST_IMAGE_WIDTH  - 30])
+@pytest.mark.parametrize('use_forward', [False]) # TODO add True afte implementing
+def test_resize(test_img, h, w, use_forward):
+    img = test_img.copy()
+    results = resize(img, h ,w, use_forward)
+    resized = results['resized']
+    horizontal_seams_viz = results['horizontal_seams']
+    vertical_seams_viz   = results['vertical_seams']
+    assert resized.shape == (h, w, 3) # the requested size
+    assert vertical_seams_viz.shape   == (TEST_IMAGE_HEIGHT, TEST_IMAGE_WIDTH, 3)
+    assert horizontal_seams_viz.shape == (TEST_IMAGE_HEIGHT, w, 3)
