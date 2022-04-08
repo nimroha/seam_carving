fix off-by-one shift in forward energy

seam_carving.py
@@ -31,8 +31,8 @@ def vertical_cost(grad, intensity, use_forward):
     energy_pad = np.column_stack([inf_column, energy_pad, inf_column])
 
     if use_forward: # pad intensity as well
-        # mirror the top row to avoid considering "phantom" edges at the top of the image # TODO necessary?
-        intensity_pad = np.row_stack([intensity[0], intensity])
+        # it doesn't matter what we pad with, we're going to zero out the top row anyway
+        intensity_pad = np.row_stack([zero_row, intensity])
 
         # pad infinity columns from the left and right
         intensity_pad = np.column_stack([inf_column, intensity_pad, inf_column])
@@ -65,10 +65,17 @@ def compute_new_edge_cost(intensity):
     y_left_edge  = np.abs(np.roll(intensity, -1, axis=0) - np.roll(intensity, -1, axis=1))
     y_right_edge = np.abs(np.roll(intensity, -1, axis=0) - np.roll(intensity,  1, axis=1))
 
+    y_left_edge  = np.roll(np.roll(y_left_edge,  1, axis=0),  1, axis=1)
+    y_right_edge = np.roll(np.roll(y_right_edge, 1, axis=0), -1, axis=1)
+
     # remove NaNs
     x_edge      [np.isnan(x_edge)]       = np.inf
     y_left_edge [np.isnan(y_left_edge)]  = np.inf
     y_right_edge[np.isnan(y_right_edge)] = np.inf
+
+    # remove cost from padding and top row (no new edges created)
+    y_left_edge [:2] = 0
+    y_right_edge[:2] = 0
 
     return x_edge, y_left_edge, y_right_edge
 

tests.py
@@ -47,7 +47,7 @@ def test_remove_seam():
 
 @pytest.mark.parametrize('h', [TEST_IMAGE_HEIGHT, TEST_IMAGE_HEIGHT + 30, TEST_IMAGE_HEIGHT - 30])
 @pytest.mark.parametrize('w', [TEST_IMAGE_WIDTH,  TEST_IMAGE_WIDTH  + 30, TEST_IMAGE_WIDTH  - 30])
-@pytest.mark.parametrize('use_forward', [False]) # TODO add True afte implementing
+@pytest.mark.parametrize('use_forward', [False, True])
 def test_resize(test_img, h, w, use_forward):
     img = test_img.copy()
     results = resize(img, h ,w, use_forward)
@@ -66,7 +66,7 @@ def test_compute_new_edge_cost():
                           [ 9,  9, 12, 19,  9, 11],
                           [16, 13, 18, 19,  8, 19]], dtype=np.int32)
 
-    padded  = np.row_stack([intensity[0], intensity])
+    padded  = np.row_stack([np.zeros(intensity.shape[1]), intensity])
     inf_col = np.full(padded.shape[1], fill_value=np.inf)
     padded  = np.column_stack([inf_col, padded, inf_col])
     x, left_y, right_y = compute_new_edge_cost(padded)
@@ -87,6 +87,9 @@ def test_compute_new_edge_cost():
         for j in range(1, padded.shape[1] - 1):
             expected_right_y[i, j] = np.abs(padded[i - 1, j] - padded[i, j + 1])
 
+    expected_left_y [:2] = 0 # zero out padding and first row
+    expected_right_y[:2] = 0
+
     assert np.all(x      [1:, 1:-1] == expected_x      [1:, 1:-1])
-    assert np.all(left_y [1:, 1:-1] == expected_left_y [1:, 1:-1]) # TODO shifted results
-    assert np.all(right_y[1:, 1:-1] == expected_right_y[1:, 1:-1]) # TODO shifted results
+    assert np.all(left_y [1:, 1:-1] == expected_left_y [1:, 1:-1])
+    assert np.all(right_y[1:, 1:-1] == expected_right_y[1:, 1:-1])